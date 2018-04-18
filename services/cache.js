e2396ff0098f4cfe28f4778f3474cc98ac1029d5
Alex Steele <alexsteele110@gmail.com>
2018-04-18 15:26:55 -0400

    finished basic redis cache system

--- services/cache.js
+++ services/cache.js
@@ -4,13 +4,15 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function() {
+mongoose.Query.prototype.cache = function(options = {}) {
   this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
+
   return this;
-}
+};
 
 mongoose.Query.prototype.exec = async function() {
   if (!this.useCache) {
@@ -22,7 +24,7 @@ mongoose.Query.prototype.exec = async function() {
     collection: this.mongooseCollection.name
   });
 
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   if (cacheValue) {
     const doc = JSON.parse(cacheValue);
@@ -34,7 +36,13 @@ mongoose.Query.prototype.exec = async function() {
 
   const result = await exec.apply(this, arguments);
 
-  client.set(key, JSON.stringify(result));
+  client.hmset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
 
   return result;
-}
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  }
+};
